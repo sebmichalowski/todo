cc1903171f4f62d5b6c4540eb94f6a716f6da5b0
sebmichalowski <seb.michalowski@gmail.com>
2016-11-04 00:57:10 +0100

    endless loop

--- todo.py
+++ todo.py
@@ -5,30 +5,38 @@ commands = ['add', 'mark', 'list', 'archive', 'x']
 todo_list = [0]
 
 you_saved = 'You saved the following to-do items: \n'
+global outprint_menu
 outprint_menu = input('Please specify a command [list, add, mark, archive]: ')
 
-
 def menu(give_outprint_menu):
-        if give_outprint_menu == 'add':
-            add()
-        elif give_outprint_menu == 'mark':
-            mark()
-        elif give_outprint_menu == 'list':
-            return listing_tasks(todo_list)
-        elif give_outprint_menu == 'archive':
-            pass
-        elif give_outprint_menu == 'x':
-            return quit()
-
-        else:
-            print("I can't understant you.")
-            time.sleep(1)
-            #menu()
+    if give_outprint_menu == 'add':
+        add()
+        return main()
+    elif give_outprint_menu == 'mark':
+        mark()
+    elif give_outprint_menu == 'list':
+        return listing_tasks(todo_list)
+    elif give_outprint_menu == 'archive':
+        pass
+    elif give_outprint_menu == 'x':
+        return quit()
+    else:
+        print("I can't understant you.")
+        time.sleep(1)
+        return main()
 
 
 def add():
     todo_list.append(input('Add an item: '))
-
+    add_next = (input('add another one? y/n'))
+    if add_next == 'y':
+        add()
+    elif add_next == 'n':
+        main()
+    else:
+        print("I can't understant you.")
+        time.sleep(1)
+    return main()
 
 def mark():
     pass
@@ -47,6 +55,7 @@ def user_list_print(give_list):
 
 def main():
     outprint_menu
-    menu()
+    menu(outprint_menu)
+
 
 main()
