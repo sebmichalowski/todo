74c9fe2245b282d457086ab44fc17e9f63e00c4a
sebmichalowski <seb.michalowski@gmail.com>
2016-11-04 00:02:12 +0100

    not even close

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-inport sys
+import sys
 
 commands = ['add', 'mark', 'list', 'archive', 'x']
 todo_list = [0]
@@ -7,41 +7,41 @@ you_saved = 'You saved the following to-do items: \n'
 outprint_menu = input('Please specify a command [list, add, mark, archive]: ')
 
 
-def menu(arg):
-    outprint_menu
-        if arg == 'add':
+def menu(give_outprint_menu):
+        if give_outprint_menu == 'add':
             add()
-        elif arg == 'mark':
+        elif give_outprint_menu == 'mark':
             mark()
-        elif arg == 'list':
-
-        elif arg == 'archive':
+        elif give_outprint_menu == 'list':
+            return listing_tasks(arg)
+        elif give_outprint_menu == 'archive':
             pass
-        elif arg == 'x':
-            quit()
+        elif give_outprint_menu == 'x':
+            return quit()
+
 
 
 def add():
     todo_list.append(input('Add an item: '))
 
+
 def mark():
 
 
 
-def listing_tasks(arg):
+def listing_tasks(give_saved_list):
     print(you_saved)
-    user_list_print(arg)
-    menu(arg)
+    user_list_print(give_saved_list)
 
 
-def user_list_print(arg):
-    for item in range(len(arg)):
+def user_list_print(give_list):
+    for item in range(len(give_list)):
         print('[]''{}' .format(,arg[item]))
 
 
 def main():
+    outprint_menu
     add()
-    print(todo_list)
     listing_tasks(todo_list)
 
 main()
